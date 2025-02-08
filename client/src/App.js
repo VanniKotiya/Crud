96c4fe4278f013cd0ff2a4b261fd523c5d0e5f2f
Pethum Chamara Vanniarachchi <pathumc4@gmail.com>
2025-02-08 21:49:52 +0530

    created components and nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,77 +1,28 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from './components/Home';
+import CreatePost from './components/CreatePost';
+import EditPost from './components/EditPost';
+import PostDetails from './components/PostDetails';
+import NavBar from './components/NavBar';
 
 export default class App extends Component {
-constructor(props){
-  super(props);
-
-  this.state={
-    posts:[]
-  };
-}
-
-componentDidMount(){
-  this.retrivePosts();
-}
-
-retrivePosts(){
-  axios.get("http://localhost:8000/post").then(res =>{
-    if (res.data.success){
-      this.setState({
-        posts:res.data.existingPosts 
-      });
-
-      console.log(this.state.posts);
-    }
-  });
-}
-
   render() {
     return (
-      <div className ="container">
-        <p>All posts</p>
-        <table class = "table">
-          <thead>
-            <tr>
-              <th scope ="col">#</th>
-              <th scope ="col">Topic</th>
-              <th scope ="col">Description</th>
-              <th scope ="col">Post Catergory</th>
-              <th scope ="col">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.posts.map((posts,index) =>(
-              <tr>
-                <th scope ="row">{index+1}</th>
-                <td>{posts.topic}</td>
-                <td>{posts.description}</td>
-                <td>{posts.postCatergory}</td>
-                <td>
-                  <a className="btn btn-warning" href="#">
-                    <i className='fas fa-edit'></i>&nbsp;Edit
-                  </a>
-                  &nbsp;               
-                  <a className="btn btn-danger" href="#">
-                    <i className='far fa-trash-alt'></i>&nbsp;Delete
-                  </a>
-                </td>
-              </tr>
+      <BrowserRouter>
+        <div className='container'>
+          <NavBar/> 
+          <Routes>
+            <Route path='/' element={<Home/>}></Route>
+            <Route path='/add' element={<CreatePost/>}></Route>
+            <Route path='/edit/:id' element={<EditPost/>}></Route>
+            <Route path='/post/:id' element={<PostDetails/>}></Route>
+          </Routes> 
+        </div>
+      
+      </BrowserRouter>
 
-            ))}
-          </tbody>
-        </table>
-
-        {/* {this.state.posts.map(posts =>(
-            <div>
-              <p>{posts.topic}</p>
-              <p>{posts.description}</p>
-              <p>{posts.postCatergory}</p>
-            </div>
-        ))} */}
-
-      </div>
+      
     )
   }
 }
---
+++ client/src/components/Home.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';  // ✅ Import Link
+
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: [],
+    };
+  }
+
+  componentDidMount() {
+    this.retrivePosts();
+  }
+
+  retrivePosts() {
+    axios.get("http://localhost:8000/post").then((res) => {
+      if (res.data.success) {
+        this.setState({
+          posts: res.data.existingPosts,
+        });
+
+        console.log(this.state.posts);
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <p>All posts</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Topic</th>
+              <th scope="col">Description</th>
+              <th scope="col">Post Category</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.posts.map((post, index) => (
+              <tr key={post._id}>
+                <th scope="row">{index + 1}</th>
+                <td>
+                  <Link to={`/post/${post._id}`} style={{ textDecoration: 'none' }}>
+                    {post.topic}
+                  </Link>
+                </td>
+                <td>{post.description}</td>
+                <td>{post.postCategory}</td>
+                <td>
+                  <button className="btn btn-warning">
+                    <i className="fas fa-edit"></i>&nbsp;Edit
+                  </button>
+                  &nbsp;
+                  <button className="btn btn-danger">
+                    <i className="far fa-trash-alt"></i>&nbsp;Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <Link to="/add" className="btn btn-success">Create New Post</Link>
+      </div>
+    );
+  }
+}
