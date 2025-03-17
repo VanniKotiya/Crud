7c8919bed0033135957e1779e7cbd291c36b14af
Pethum Chamara Vanniarachchi <pathumc4@gmail.com>
2025-03-17 15:16:57 +0530

    done edit Post and Delete Post

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -21,15 +21,28 @@ export default class Home extends Component {
         this.setState({
           posts: res.data.existingPosts,
         });
-
-        console.log(this.state.posts);
       }
-    });
+    }).catch((err) => console.error("Error fetching posts:", err));
   }
+
+  // ✅ Handle Delete Function
+  handleDelete = (id) => {
+    const confirmDelete = window.confirm("Are you sure you want to delete this post?");
+    if (!confirmDelete) return;
+
+    axios.delete(`/post/delete/${id}`)
+      .then((res) => {
+        if (res.data.success) {
+          alert("Post Deleted Successfully!");
+          this.retrivePosts(); // 🔄 Refresh the posts list
+        }
+      })
+      .catch((err) => console.error("Error deleting post:", err));
+  };
 
   render() {
     return (
-    <div>
+      <div>
         <p>All posts</p>
         <table className="table">
           <thead>
@@ -37,13 +50,13 @@ export default class Home extends Component {
               <th scope="col">#</th>
               <th scope="col">Topic</th>
               <th scope="col">Description</th>
-              <th scope="col">Post Catergory</th>
+              <th scope="col">Post Category</th>
               <th scope="col">Action</th>
             </tr>
           </thead>
           <tbody>
             {this.state.posts.map((post, index) => (
-              <tr key={index}>
+              <tr key={post._id}>
                 <th scope="row">{index + 1}</th>
                 <td>
                   <Link to={`/post/${post._id}`} style={{ textDecoration: 'none' }}>
@@ -53,11 +66,14 @@ export default class Home extends Component {
                 <td>{post.description}</td>
                 <td>{post.postCatergory}</td>
                 <td>
-                  <button className="btn btn-warning">
+                  <Link to={`/edit/${post._id}`} className="btn btn-warning">
                     <i className="fas fa-edit"></i>&nbsp;Edit
-                  </button>
+                  </Link>
                   &nbsp;
-                  <button className="btn btn-danger">
+                  <button 
+                    className="btn btn-danger" 
+                    onClick={() => this.handleDelete(post._id)}
+                  >
                     <i className="far fa-trash-alt"></i>&nbsp;Delete
                   </button>
                 </td>
@@ -66,7 +82,7 @@ export default class Home extends Component {
           </tbody>
         </table>
         <Link to="/add" className="btn btn-success">Create New Post</Link>
-    </div>
+      </div>
     );
   }
 }
